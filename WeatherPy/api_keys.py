29090d237d39199b1cc650b39ef39cef00f5b78e
Mark McPherson <marksmcpherson@icloud.com>
2020-04-30 18:27:59 -0400

    add close to final

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "9070664c8a5d40514d553614ff63c16a"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCQ1kv8abMW2IDI3rQ7NVoZSAnCAclKMCw"
